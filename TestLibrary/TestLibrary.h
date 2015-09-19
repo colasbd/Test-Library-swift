8812396b7686ed99511cf9086c001887af43d0af
Colas <colasjojo@yahoo.fr>
2015-09-19 11:34:56 -0400

    Initial Commit

---
+++ TestLibrary/TestLibrary.h
@@ -0,0 +1,19 @@
+//
+//  TestLibrary.h
+//  TestLibrary
+//
+//  Created by Colas on 19/09/2015.
+//  Copyright (c) 2015 colas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestLibrary.
+FOUNDATION_EXPORT double TestLibraryVersionNumber;
+
+//! Project version string for TestLibrary.
+FOUNDATION_EXPORT const unsigned char TestLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestLibrary/PublicHeader.h>
+
+
