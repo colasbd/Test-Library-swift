0dcbeb8e8bcbcb0b157431b80c795f694ebf566f
Colas <colasjojo@yahoo.fr>
2015-09-22 08:35:20 -0400

    first commit

---
+++ TestLibrary/Person.swift
@@ -0,0 +1,15 @@
+//
+//  MyClass.swift
+//  Test-Library
+//
+//  Created by Colas on 19/09/2015.
+//  Copyright (c) 2015 colas. All rights reserved.
+//
+
+
+class MyClass {
+    
+    var name : String = ""
+    private var nickname : String = ""
+    
+}
